fe131996a8a7e869c6dd6b22f45f831b4869115d
jmcneive <jerry.mcneive@gmail.com>
2015-01-26 08:38:15 -0700

    Build columns

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,35 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    plumber = require('gulp-plumber');
+var gulp = require('gulp');
+    
+var sass = require('gulp-sass');
+var takana = require('takana');
+var plumber = require('gulp-plumber');
 
-    // Scripts task
-    // Uglifies
-    gulp.task('scripts', function() {
-      gulp.src('assets/js/*.js')
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(gulp.dest('build/js'));  
+    // Run Takana
+    gulp.task('takana', function() {
+      takana.run({
+        path:         __dirname, // run from the current working directory
+        includePaths: []         // optional include paths
+      });
     });
 
-    // Styles task
-    // Drugs
+    // Compile SCSS
     gulp.task('sass', function() {
-      gulp.src('assets/sass/**/**/**/*.scss')
+      gulp.src('./assets/sass/**/**/**/*.scss')
         .pipe(plumber())
         .pipe(sass({
           style: 'nested',
           lineNumbers: true
     }))
-      .pipe(gulp.dest('assets/css/'));
+      .pipe(gulp.dest('./assets/css/'));
     });
 
     // Watch task
     // Watches JS
     gulp.task('watch', function() {
-        gulp.watch('assets/js/*.js', ['scripts']);
-        gulp.watch('assets/sass/**/**/**/*.scss', ['sass']);
+        gulp.watch('./assets/js/*.js', ['scripts']);
+        gulp.watch('./assets/sass/**/**/**/*.scss', ['sass']);
     });
 
-    gulp.task('default', ['sass', 'scripts', 'watch']);
+    gulp.task('default', ['takana', 'sass', 'watch']);
 
 
